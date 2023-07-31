dc2c02fa9af48458ff5e64042fa5e08cd3b3121b
KIPTOYOT BRIAN <ngenobrayo7@gmail.com>
2023-07-31 16:34:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -157,20 +157,7 @@ const createTimeOutEvent = function (dateStamp) {
 
   };
 
-  /*
-
- We're giving you this function. Take a look at it, you might see some usage
-
- that's new and different. That's because we're avoiding a well-known, but
-
- sneaky bug that we'll cover in the next few lessons!
-
-
- As a result, the lessons for this function will pass *and* it will be available
-
- for you to use if you need it!
-
- */
+ 
 
 
 const allWagesFor = function () {
@@ -191,8 +178,7 @@ const allWagesFor = function () {
 
     0
 
-  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
+  ); 
   return payable;
 
 };
